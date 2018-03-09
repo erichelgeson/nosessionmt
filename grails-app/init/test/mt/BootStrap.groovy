8cb21600e02ca35d45eab327d509c29ec48f0a73
Eric Helgeson <eric@agileorbit.com>
2018-03-09 08:25:00 -0600

    Testing

--- grails-app/init/test/mt/BootStrap.groovy
+++ grails-app/init/test/mt/BootStrap.groovy
@@ -1,9 +1,40 @@
 package test.mt
 
+import grails.gorm.multitenancy.Tenants
+import grails.gorm.multitenancy.WithoutTenant
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
 
     def init = { servletContext ->
+        create()
+        listAllPeople()
     }
+
+    @WithoutTenant
+    @Transactional
+    def listAllPeople() {
+        Person.findAll().each {
+            println "$it.name $it.tenantId"
+        }
+    }
+
+    @Transactional
+    def create() {
+        def tenant = new Tenant(name:"tenant1").save(failOnError:true)
+        def tenantId = tenant.id
+        Tenants.withId(tenantId) {
+            new Person(name:"me1", tenantId: tenantId).save()
+        }
+
+        def tenant2 = new Tenant(name:"tenant2").save(failOnError:true)
+        def tenant2Id = tenant2.id
+        Tenants.withId(tenant2Id) {
+            new Person(name:"me2").save()
+        }
+    }
+
+
     def destroy = {
     }
 }
